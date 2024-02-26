02e9e4b06a5582c4f85702c4a9b2f693e596ad6b
CriticalTim <105226272+CriticalTim@users.noreply.github.com>
2024-02-26 10:27:41 +0100

    Projektdateien hinzufügen.

---
+++ SafeChat/SafeChat/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SafeChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message) 
+        { 
+            await Clients.All.SendAsync("RecieveMessage", user, message);
+        }
+
+        public async Task IsConnected(string user)
+        {
+            await Clients.All.SendAsync("ConnectionSuccess", user);
+        }
+    }
+}
